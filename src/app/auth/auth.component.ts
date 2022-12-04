3d5ebae51fc586f4eef991d753e3c85db8faef7d
ManitKumarPanda <59570440+Manit-Kumar-Panda@users.noreply.github.com>
2022-12-05 01:45:23 +0530

    wrong creds pop using #2 Imperative  way

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,20 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
+import { AlertComponent } from "../shared/alert/alert.component";
+import { PlaceHolderDirective } from "../shared/placeHolder/placeholder.directive";
 import { AuthService, responseData } from "./auth.service";
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
 
-    constructor(private authService: AuthService, private router: Router) { }
+    constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver) { }
 
     isLoginMode = true;
     isLoading = false;
     error: String = null;
+    @ViewChild(PlaceHolderDirective, { static: false }) alertHost: PlaceHolderDirective;
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -43,6 +46,7 @@ export class AuthComponent {
             this.router.navigate(['/recipes']);
         }, errorRes => {
             this.error = errorRes;
+            this.showErrorAlert(errorRes);
             this.isLoading = false;
         });
 
@@ -51,5 +55,25 @@ export class AuthComponent {
 
     onHandleError() {
         this.error = null;
+    }
+
+    private closeSub: Subscription;
+    private showErrorAlert(message: string) {
+        // const alertComp = new AlertComponent(); // this is not it , angular does other thing too like associate template to it and add it to change detection which won't work if we create it like this
+        const alertComponentFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+        const hostViewContainerRef = this.alertHost.viewContainerRef;
+        hostViewContainerRef.clear();
+        const componentRef = hostViewContainerRef.createComponent(alertComponentFactory);
+        componentRef.instance.message = message;
+        this.closeSub = componentRef.instance.close.subscribe(() => {
+            hostViewContainerRef.clear();
+            this.closeSub.unsubscribe();
+        })
+    }
+
+    ngOnDestroy(): void {
+        if (this.closeSub) {
+            this.closeSub.unsubscribe();
+        }
     }
 }
